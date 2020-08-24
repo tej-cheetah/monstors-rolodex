34e2174024607ce6fc8912c08377642a624f49ce
Tejas Neema <tejas.neema@BRILLIO.COM>
2020-08-24 16:34:31 +0530

    Updates

---
+++ precache-manifest.935cbfb24eddd13b0dfcab7128f19e60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9981954ea8581aa5d1beb23e27f17f6d",
+    "url": "/monstors-rolodex/index.html"
+  },
+  {
+    "revision": "95e36c9b78d584b25806",
+    "url": "/monstors-rolodex/static/css/main.3d7e6bc9.chunk.css"
+  },
+  {
+    "revision": "2348cf9be702e2f66f64",
+    "url": "/monstors-rolodex/static/js/2.38a10f14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstors-rolodex/static/js/2.38a10f14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e36c9b78d584b25806",
+    "url": "/monstors-rolodex/static/js/main.1315df78.chunk.js"
+  },
+  {
+    "revision": "3166440b5c3dd3332041",
+    "url": "/monstors-rolodex/static/js/runtime-main.39cc9c93.js"
+  }
+]);
